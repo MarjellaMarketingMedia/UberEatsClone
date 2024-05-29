453cac6d91c8da777e6288a6852dd2e52e9c0234
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-05-29 17:38:01 +0200

    FREE YSL

--- app/src/main/java/com/example/ubereatsclone/MainActivity.java
+++ app/src/main/java/com/example/ubereatsclone/MainActivity.java
@@ -2,13 +2,9 @@ package com.example.ubereatsclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ArrayList<Product> products = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
